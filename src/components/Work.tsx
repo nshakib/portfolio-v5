a75b47f3662c78f43f7131e77507cec01d3a98b5
nshakib <nazmus.shakib.cse@gmail.com>
2026-03-17 14:10:32 +0600

    bug-fixed: solve framer-motion Variants issue

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,9 +1,9 @@
-import { motion } from 'framer-motion'
+import { motion, type Variants } from 'framer-motion'
 import { projects } from '../assets/assets'
 import { FaArrowRight, FaChevronDown } from 'react-icons/fa'
 const Work = () => {
   // variants
-  const containerVariants ={
+  const containerVariants:Variants = {
     hidden:{},
     show:{
       transition:{
@@ -12,16 +12,16 @@ const Work = () => {
     },
   }
 
-  const cardVariants = {
+  const cardVariants:Variants = {
     hidden:{opacity:0, y:60},
     show:{opacity:1, y:0, transition:{duration:0.6}}
   }
 
-  const linkVariants = {
+  const linkVariants:Variants = {
     hover:{x:6, transition:{type:"spring", stiffness:300}}
   }
 
-  const buttonVariants = {
+  const buttonVariants:Variants = {
     hover:{scale:1.06, transition:{type:"spring", stiffness:300}}
   }
   return (
